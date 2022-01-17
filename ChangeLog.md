359e2945b01d8a27159232b1d457a1988f2f6089
Fabian Bull <fabian@fbull.de>
2022-01-17 19:01:37 +0100

    Build the project with nix
    - The stack setup still works

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for wm
-
-## Unreleased changes
---
+++ xmonad.nix
@@ -0,0 +1,23 @@
+{ mkDerivation, base, containers, hpack, lib, X11, xmonad
+, xmonad-contrib, xmonad-spotify
+}:
+mkDerivation {
+  pname = "wm";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    base containers X11 xmonad xmonad-contrib xmonad-spotify
+  ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    base containers X11 xmonad xmonad-contrib xmonad-spotify
+  ];
+  testHaskellDepends = [
+    base containers X11 xmonad xmonad-contrib xmonad-spotify
+  ];
+  prePatch = "hpack";
+  homepage = "https://github.com/peri4n/wm#readme";
+  license = lib.licenses.bsd3;
+}
